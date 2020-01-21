c372644caf2459835685a63f275185db502b2c33
Liubou Tserakhava <Liubou.Tserakhava@itechart-group.com>
2020-01-21 16:19:21 +0300

    fixed

--- tasks/seven-task/utils.js
+++ tasks/seven-task/utils.js
@@ -1,6 +1,6 @@
 const path = (arrOfKeys, obj) => {
   if (!Array.isArray(arrOfKeys) || typeof obj !== 'object') throw new Error('Invalid argument type');
-  let newObj = { ...obj };
+  let newObj = obj;
   for (let i = 0; i < arrOfKeys.length; i += 1) {
     newObj = newObj[arrOfKeys[i]];
   }
